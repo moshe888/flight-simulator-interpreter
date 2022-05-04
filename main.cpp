fe36c090f59caebc7854b0abd1e74ab7cadf5f78
moshe <sayadamoshe@gmail.com>
2022-05-04 19:31:16 +0300

    1

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include "lexer.h"
+
+int main() {
+    Lexer lexer;
+    /*string line;
+    std::cout << "  insert line: ";
+    std::getline(std::cin , line);  
+    std::cout << "  is: " << line << std::endl;
+ 
+    lexer.parser_line(line);
+    sleep(20);
+    lexer.parser_line("connect 127.0.0.1 5402");*/
+
+    lexer.parser_file("file1.txt");
+
+    ((Connect*) lexer.interpreter.map["connect"])->Send("ls\r\n");
+    sleep(1);
+    ((Connect*) lexer.interpreter.map["connect"])->recieve();
+    sleep(1);
+
+
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder 1\r\n");
+    sleep(1);
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder -1\r\n");
+    sleep(1);
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder 1\r\n");
+    sleep(1);
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder -1\r\n");
+    sleep(1);
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder 1\r\n");
+    sleep(1);
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder -1\r\n");
+    sleep(1);
+    ((Connect*)(lexer.interpreter.map["connect"]))->Send("set controls/flight/rudder 1\r\n");
+
+}
+
