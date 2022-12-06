293e503135b9692ae5cd58ed9b2b44a146db461e
moshe <sayadamoshe@gmail.com>
2022-12-06 22:04:24 +0200

    end day

---
+++ Tools.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+class Tools
+{
+ public:
+ vector<string>  split(string str, const string& delimiter)
+{
+	vector<string> result;
+
+	size_t pos = 0;
+	string token;
+	while ((pos = str.find(delimiter)) != string::npos)
+	{
+		token = str.substr(0, pos);
+		while (token[0] == ' ' || token[0] == '\t' || token[0] == '\r' || token[0] == '\n') {
+			token.erase(token.begin());
+		}
+		if (token != "") {
+			result.push_back(token);
+		}
+		str.erase(0, pos + delimiter.length());
+	}
+	
+	result.push_back(str);
+
+	return result;
+}
+
+};
